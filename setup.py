f98ca494c5f1e39fcc70785b8e7803409ba0b6ac
Vietor Liu <vietor.liu@gmail.com>
2016-09-16 17:36:25 +0800

    Modify for PyPI support

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.7.3'
+
+
+setup(
+    name='weaknet',
+    version=VERSION,
+    description='A limited network transport implementation',
+    keywords='weaknet, shadowsocks, ss',
+    author='Vietor Liu',
+    author_email='vietor.liu@gmail.com',
+    url='https://github.com/vietor/pyweaknet',
+    license='MIT',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    requires=[
+        'python (>=2.6.0)',
+    ],
+    entry_points={
+        'console_scripts': [
+            'weaknet = weaknet.weaknet:main',
+        ]
+    },
+    install_requires=[],
+    data_files=[],
+)
