64be73b47acd501fb7ff0a71ad2e7af9a07f802c
Vietor Liu <vietor.liu@gmail.com>
2016-10-03 09:20:17 +0800

    Modify scripts

--- test/start_local.sh
+++ test/start_local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../weaknet.py -r local -s demo -R 127.0.0.1 -w 1
+python ../weaknet.py -r local -s demo -R 127.0.0.1 -w 1 $@
--- test/start_remote.sh
+++ test/start_remote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../weaknet.py -r remote -s demo -w 1
+python ../weaknet.py -r remote -s demo -w 1 $@
